47cd25888ebaeb12694faea89e45778d51e22539
silso <chroelo@gmail.com>
2018-12-19 04:30:12 -0600

    adjusted cell style, working on clicking

--- src/App.js
+++ src/App.js
@@ -6,12 +6,38 @@ import './styles/styles.scss';
  * App
  */
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      count: 0,
+      mouseDownCount: 0,
     };
+		this.handleClick = this.handleClick.bind(this);
+		this.handleUnclick = this.handleUnclick.bind(this);
   }
+
+	handleClick(e) {
+		if (e.button === 0) {
+			this.setState({mouseDownCount: this.state.mouseDownCount + 1});
+			console.log(this.state.mouseDownCount);
+		}
+	}
+
+	handleUnclick(e) {
+		if (e.button === 0) {
+			this.setState({mouseDownCount: this.state.mouseDownCount - 1});
+			console.log(this.state.mouseDownCount);
+		}
+	}
+
+	componentDidMount() {
+		document.body.addEventListener('mousedown', this.handleClick);
+		document.body.addEventListener('mouseup', this.handleUnclick);
+	}
+
+	componentWillUnmount() {
+		document.body.removeEventListener('mousedown', this.handleClick);
+		document.body.removeEventListener('mouseup', this.handleUnclick);
+	}
 
   render() {
 		return (
@@ -30,4 +56,5 @@ class App extends React.Component {
 		);
   }
 }
+
 export default App;
--- src/PatTable.js
+++ src/PatTable.js
@@ -6,11 +6,18 @@ import React from 'react';
 class PatCell extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			color: this.props.color,
+		};
+	}
+
+	handleClick() {
+		this.setState({color: "red"});
 	}
 
 	render() {
 		return (
-			<td className={this.props.className} style={{backgroundColor: this.props.color}}></td>
+			<td className={this.props.className} onClick={() => this.handleClick()} style={{backgroundColor: this.state.color, borderRight: (this.props.border ? "1px solid black" : "none")}}></td>
 		)
 	}
 }
@@ -28,11 +35,13 @@ class PatTable extends React.Component {
 		for (let i = 0; i < this.props.numY; i++) {
 			let row = new Array(this.props.numX);
 			for (let j = 0; j < this.props.numX; j++) {
+				/*
 				let c = [];
 				for (let k = 0; k < 3; k++) {
 					c[k] = Math.floor(Math.random() * 256);
 				}
-				row[j] = <PatCell key={`${i},${j}`} className="patCell" color={/*`rgb(${c[0]},${c[1]},${c[2]})`*/"white"}/>;
+				*/
+				row[j] = <PatCell key={`${i},${j}`} className="patCell" color={/*`rgb(${c[0]},${c[1]},${c[2]})`*/"white"} border={Math.floor(Math.random()*2)}/>;
 			}
 			table[i] = <tr key={`${i}`}>{row}</tr>;
 		}

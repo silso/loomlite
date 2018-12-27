2873300400ca5a2ea99095f795915b8d2f159ad0
silso <chroelo@gmail.com>
2018-12-26 20:10:29 -0600

    update warpColors and weftColors Draft state appropriately

--- src/Draft.js
+++ src/Draft.js
@@ -3,7 +3,25 @@ import {Coloring, Tieup, Threading, Treadling, Drawdown} from './Tables.js';
 
 const initShafts = 4, initTreadles = 6, initWarp = 50, initWeft = 50;
 
-export default class Draft extends React.Component {
+/**
+ * updateArray
+ * @param a input array to be updated
+ * @param i index of element in array a to be updated
+ * @param fn function to be called on element a[i]
+ */
+
+function updateArray(a, i, fn) {
+	return a.map((item, j) => {
+		if (j === i) {
+			return fn(item);
+		}
+		else {
+			return item;
+		}
+	});
+}
+
+export default class Draft extends React.PureComponent {
 	constructor(state) {
 		super(state);
 		this.state = {
@@ -11,22 +29,25 @@ export default class Draft extends React.Component {
 			treadles: initTreadles,
 			warp: initWarp,
 			weft: initWeft,
+			cursorColor: 1,
 			warpColors: new Array(initWarp).fill(0),
 			weftColors: new Array(initWeft).fill(0)
 		}
+		this.handleClickWarpColor = this.handleClickWarpColor.bind(this);
+		this.handleClickWeftColor = this.handleClickWeftColor.bind(this);
 	}
 
 	handleClickWarpColor(i, j) {
 		this.setState(state => {
-			const newWarpColors = state.warpColors.map((color, k) => {
-				if (k === i) {
-					return color + 1;
-				}
-				else {
-					return color;
-				}
-			});
+			const newWarpColors = updateArray(state.warpColors, j, a => state.cursorColor);
 			return {warpColors: newWarpColors};
+		});
+	}
+
+	handleClickWeftColor(i, j) {
+		this.setState(state => {
+			const newWeftColors = updateArray(state.weftColors, i, a => state.cursorColor);
+			return {weftColors: newWeftColors};
 		});
 	}
 
@@ -37,7 +58,7 @@ export default class Draft extends React.Component {
 					<tr>
 						<td></td>
 						<td></td>
-						<td><Coloring numX={this.state.warp} numY="1" onClick={this.handleClickWarpColor.bind(this)}/></td>
+						<td><Coloring numX={this.state.warp} numY="1" onClick={this.handleClickWarpColor}/></td>
 					</tr>
 					<tr>
 						<td></td>
@@ -45,7 +66,7 @@ export default class Draft extends React.Component {
 						<td><Threading numX={this.state.warp} numY={this.state.shafts}/></td>
 					</tr>
 					<tr>
-						<td><Coloring numX="1" numY={this.state.weft}/></td>
+						<td><Coloring numX="1" numY={this.state.weft} onClick={this.handleClickWeftColor}/></td>
 						<td><Treadling numX={this.state.treadles} numY={this.state.weft}/></td>
 						<td><Drawdown numX={this.state.warp} numY={this.state.weft}/></td>
 					</tr>
